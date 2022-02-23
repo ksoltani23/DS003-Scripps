a2f325047a49f0ac7d79316899615ca8c2751741
ksoltani23 <98316205+ksoltani23@users.noreply.github.com>
2022-02-23 12:06:43 -0800

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -3,8 +3,8 @@ num_friends = [100.0,49,41,40,25,21,21,19,19,18,18,16,15,15,15,15,14,14,13,13,13
 from collections import Counter
 import matplotlib.pyplot as plt
 
-from dgoodwin.linear_algebra import sum_of_squares
-from dgoodwin.linear_algebra import dot
+from ksoltani23.linear_algebra import sum_of_squares
+from ksoltani23.linear_algebra import dot
 
 friend_counts = Counter(num_friends)
 xs = range(101)                         # largest value is 100
